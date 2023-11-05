09720254a1a7e450494c0813b7549ecf73a330d4
RubenIbarra0 <107138340+RubenIbarra0@users.noreply.github.com>
2023-11-05 13:45:28 -0600

    LastAct

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-let menuNav = document.querySelectorAll('.linksSection a');
-
+const menuNav = document.querySelectorAll('.linksSection a');
 const btnTelefono = document.getElementById('menu_telefono');
-const linksSection= document.querySelector('.linksSection');
+const desplegableLinks= document.querySelector('.desplegableLinks');
 
+btnTelefono.addEventListener("click",function(event){
+    event.stopPropagation()
+    desplegableLinks.classList.toggle('show');
 
-btnTelefono.addEventListener("click",()=>{
-    linksSection.classList.toggle("show");
+});
+document.addEventListener('click', function(event){
+    if(!desplegableLinks.contains(event.target) && desplegableLinks.classList.contains('show')){
+        desplegableLinks.classList.remove('show');
+    }
+});
+menuNav.forEach(posado =>{
+    posado.addEventListener('click', function(event){
+        menuNav.forEach(posado => posado.classList.remove('active'));
+        this.classList.add('active');
+    })
 });
 
 const typed = new Typed('.typed', {
@@ -23,12 +34,5 @@ const typed = new Typed('.typed', {
     loopCount: false,
     contentType: 'html'
 });
-
-menuNav.forEach(posado =>{
-    posado.addEventListener('click', function(event){
-        menuNav.forEach(posado => posado.classList.remove('active'));
-        this.classList.add('active');
-    })
-})
 
 
